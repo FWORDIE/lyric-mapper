9d48f6d926a5452725b818cec4527b752f27afdc
Fred Wrodie <fred@mildlyupset.com>
2024-12-18 20:54:43 +0100

    NEW LOG

--- src/routes/api/lyrics/+server.ts
+++ src/routes/api/lyrics/+server.ts
@@ -20,18 +20,16 @@ export const GET = async (event) => {
             artist = event.url.searchParams.get("artist") as string;
             title = event.url.searchParams.get("title") as string;
         }
-        console.log(`${artist} - ${title}`)
-        try{
-            const searches = await Client.songs.search(`${artist} - ${title}`);
+        console.log(`${artist} - ${title}`);
 
-        }catch(e){
-            console.log(e)
-        }
+        const searches = await Client.songs.search(`${artist} - ${title}`);
+
+        console.log(searches);
         let returnData = {};
 
         // Pick first one
         const firstSong = searches[0];
-        console.log('first song:', firstSong)
+        console.log("first song:", firstSong);
         if (firstSong) {
             console.log("About the Song:\n", firstSong, "\n");
             let songData = {
@@ -43,20 +41,19 @@ export const GET = async (event) => {
             };
             returnData = songData;
             // // Ok lets get the lyrics
-            console.log('song data',songData);
+            console.log("song data", songData);
         } else {
             returnData = {
                 msg: "Could not find, sorry",
             };
         }
-        console.log('return data',returnData);
+        console.log("return data", returnData);
 
         return new Response(JSON.stringify(returnData), {
             headers: {
-                'Content-Type': 'application/json'
-            }
+                "Content-Type": "application/json",
+            },
         });
-
     } catch (e: any) {
         console.error("Error in GET function:", e);
         return json({ error: e.message || "Unknown Error" }, { status: 500 });
@@ -72,7 +69,7 @@ const top10Func = async () => {
 };
 function getChartAsPromise() {
     return new Promise((resolve, reject) => {
-        getChart((err:Error, chart:any) => {
+        getChart((err: Error, chart: any) => {
             if (err) {
                 reject(err); // Reject the Promise on error
             } else {
